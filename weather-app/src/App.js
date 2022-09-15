d72f54f42008fd411480a2cf94c2c92879dcf4b4
AJWest22 <abiwestcott@gmail.com>
2022-09-15 07:47:20 +0000

    Add function to filter records

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import CityWeather from './components/CityWeather';
 import Header from './components/Header';
+import GroupOne from './components/GroupOne'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CityWeather />
+      <GroupOne />
     </div>
   );
 }
---
+++ weather-app/src/components/GroupOne.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import info from '../weather-data.json'
+
+export default function GroupOne() {
+    const [filteredRecord, setFilteredRecord] = useState({});
+
+    useEffect(() => {
+      const record = info.data.filter((it) => it.wind_cdir === "NNW");
+      setFilteredRecord(record[0]);
+    }, []);
+  
+    return (
+      <div>
+        <p>{filteredRecord.high_temp}</p>
+        <p>{filteredRecord.wind_cdir_full}</p>
+        <p>{filteredRecord.valid_date}</p>
+        <p>{filteredRecord.uv}</p>
+      </div>
+    );
+}
