5e6468cb741d18e7f14e8bbfb055ec6146ba9288
AJWest22 <abiwestcott@gmail.com>
2022-09-15 18:29:28 +0000

    Create variables in GroupOne and style City weather

--- weather-app/src/components/CityWeather.js
+++ weather-app/src/components/CityWeather.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import info from '../weather-data.json'
 import './CityWeather.css'
+import icon from '../icons/c03d.png'
 
 // Gets the weather for current day filters through them
 export default function CityWeather() {
@@ -17,33 +18,31 @@ export default function CityWeather() {
     <div>
         {/* Optional Chaining is needed to get weather objects, marks it as undefined but doesn't question it so it loads. */}
       <div>
-        <h2>Weather for {info.city_name}. Date: {filteredRecord.valid_date}. {filteredRecord?.weather?.icon }</h2>
-        <p>Weather: {filteredRecord?.weather?.description }, {filteredRecord?.weather?.code }</p>
+        <h2>Weather for {info.city_name}</h2>
+        <h4>Date: {filteredRecord.valid_date}. {filteredRecord?.weather?.icon }. Weather: {filteredRecord?.weather?.description }, {filteredRecord?.weather?.code }</h4>
       </div>
 
-      <div>
+      <div id="alignment">
         <p>Temperature of {filteredRecord.temp} with a high temperature of {filteredRecord.high_temp} with 
             a max temperature of {filteredRecord.max_temp} with a absolute max temperature of {filteredRecord.app_max_temp}.
             A low temperature of {filteredRecord.low_temp} is expected, with a min temperature of {filteredRecord.min_temp}
             UV is expected to be {filteredRecord.uv}. Ozone is currently {filteredRecord.ozone}.
-        </p>
-        <p>The current wind direction is {filteredRecord.wind_cdir}, with the full cdir expected to be {filteredRecord.wind_cdir_full}. 
+            The current wind direction is {filteredRecord.wind_cdir}, with the full cdir expected to be {filteredRecord.wind_cdir_full}. 
             The wind direction is today expected to be from {filteredRecord.wind_dir} with gusts of {filteredRecord.wind_gust_spd}.
-        {filteredRecord.wind_spd}
-        </p>
-        <p>The high clouds are currently located at {filteredRecord.clouds_hi}, with the mid clouds at {filteredRecord.clouds_mid}. The low
-            clouds are currently located at {filteredRecord.clouds_low}. Visibility is currently {filteredRecord.vis}. Precipitation
-            is {filteredRecord.precip} with snow currently at {filteredRecord.snow}. The current snow depth is {filteredRecord.snow_depth}.
-        </p>
-        <p>The ts is currently {filteredRecord.ts}. Sunrise today is {filteredRecord.sunrise_ts} with sunset expected to be around {filteredRecord.sunset_ts}.
-            Moonrise is currently forecast for {filteredRecord.moonrise_ts} this evening, with moonset at {filteredRecord.moonset_ts} tomorrow morning. The moon is
+            {filteredRecord.wind_spd}. The high clouds are currently located at {filteredRecord.clouds_hi}, with the mid clouds at 
+            {filteredRecord.clouds_mid}. The low clouds are currently located at {filteredRecord.clouds_low}. Visibility is 
+            currently {filteredRecord.vis}. Precipitation is {filteredRecord.precip} with snow currently at {filteredRecord.snow}. 
+            The current snow depth is {filteredRecord.snow_depth}. The ts is currently {filteredRecord.ts}. Sunrise today 
+            is {filteredRecord.sunrise_ts} with sunset expected to be around {filteredRecord.sunset_ts}. Moonrise is currently forecast 
+            for {filteredRecord.moonrise_ts} this evening, with moonset at {filteredRecord.moonset_ts} tomorrow morning. The moon is
             currently in {filteredRecord.moon_phase} of its moon phase, with its moon phase lunation being {filteredRecord.moon_phase_lunation}.
+            Additional Info: The rh is: {filteredRecord.rh}. Pres is {filteredRecord.pres}. Pop is {filteredRecord.pop}. Slp is {filteredRecord.slp}. 
+            Dewpt is currently {filteredRecord.dewpt}.
         </p>
-        <p>Additional Info: The rh is: {filteredRecord.rh}. Pres is {filteredRecord.pres}. Pop is {filteredRecord.pop}. Slp is {filteredRecord.slp}. Dewpt is
-            currently {filteredRecord.dewpt}, and max dhi is {filteredRecord.max_dhi}
-        </p>
+        <img src={icon} alt="todays weather" height="150x px" width="150px"></img>
       </div>
-
+      <div>
+      </div>
     </div>
   )
 }
--- weather-app/src/components/GroupOne.js
+++ weather-app/src/components/GroupOne.js
@@ -6,16 +6,36 @@ export default function GroupOne() {
     const [filteredRecord, setFilteredRecord] = useState({});
 
     useEffect(() => {
-      const record = info.data.filter((it) => it.wind_cdir === "NNW");
+      const record = info.data.filter((it) => it.wind_dir === 238);
+      const secondRecord = info.data.filter((it) => it.wind_cdr === "WNW");
+      const thirdRecord = info.data.filter((it) => it.wind_dir === 289);
+      const fourthRecord = info.data.filter((it) => it.wind_cdir === "NW");
+      const fifthRecord = info.data.filter((it) => it.wind_dir === 320);
       setFilteredRecord(record[0]);
+      setFilteredRecord(secondRecord[0]);
+      setFilteredRecord(thirdRecord[0]);
+      setFilteredRecord(fourthRecord[0]);
+      setFilteredRecord(fifthRecord[0]);
+
     }, []);
   
     return (
       <div id="group-one">
-        <p>{filteredRecord.high_temp}</p>
-        <p>{filteredRecord.wind_cdir_full}</p>
-        <p>{filteredRecord.valid_date}</p>
-        <p>{filteredRecord.uv}</p>
+        <div>
+          <p>{filteredRecord.wind_dir}</p>
+        </div>
+        <div>
+          <p>{filteredRecord.wind_cdir_full}</p>
+        </div>
+        <div>
+          <p>{filteredRecord.wind_dir}</p>
+        </div>
+        <div>
+          <p>{filteredRecord.wind_cdir_full}</p>
+        </div>
+        <div>
+          <p>{filteredRecord.wind_dir}</p>
+        </div>
       </div>
     );
 }
