5964d142a0ba8efe87149d96a179bf61d7d76702
AJWest22 <abiwestcott@gmail.com>
2022-09-15 09:47:48 +0000

    Update section formattting for City Weather

--- weather-app/src/components/CityWeather.js
+++ weather-app/src/components/CityWeather.js
@@ -8,6 +8,7 @@ export default function CityWeather() {
 
     useEffect(() => {
       const record = info.data.filter((it) => it.wind_cdir === "NNW");
+      console.log(record[0])
       setFilteredRecord(record[0]);
     }, []);
 
@@ -21,21 +22,24 @@ export default function CityWeather() {
 
       <div>
         <p>Temperature of {filteredRecord.temp} with a high temperature of {filteredRecord.high_temp} with 
-            a max temperature of {filteredRecord.max_temp} with a absolute max temp of {filteredRecord.app_max_temp}.
+            a max temperature of {filteredRecord.max_temp} with a absolute max temperature of {filteredRecord.app_max_temp}.
+            A low temperature of {filteredRecord.low_temp} is expected, with a min temperature of {filteredRecord.min_temp}
             UV is expected to be {filteredRecord.uv}. Ozone is currently {filteredRecord.ozone}.
-            {filteredRecord.low_temp} {filteredRecord.min_temp}
         </p>
-        <p>{filteredRecord.wind_cdir}{filteredRecord.wind_cdir_full}{filteredRecord.wind_dir}{filteredRecord.wind_gust_spd}
+        <p>The current wind direction is {filteredRecord.wind_cdir}, with the full cdir expected to be {filteredRecord.wind_cdir_full}. 
+            The wind direction is today expected to be from {filteredRecord.wind_dir} with gusts of {filteredRecord.wind_gust_spd}.
         {filteredRecord.wind_spd}
         </p>
-        <p>{filteredRecord.clouds_hi}{filteredRecord.clouds_mid}{filteredRecord.clouds_low} {filteredRecord.vis}
-            {filteredRecord.precip} {filteredRecord.snow} {filteredRecord.snow_depth}
+        <p>The high clouds are currently located at {filteredRecord.clouds_hi}, with the mid clouds at {filteredRecord.clouds_mid}. The low
+            clouds are currently located at {filteredRecord.clouds_low}. Visibility is currently {filteredRecord.vis}. Precipitation
+            is {filteredRecord.precip} with snow currently at {filteredRecord.snow}. The current snow depth is {filteredRecord.snow_depth}.
         </p>
-        <p>{filteredRecord.ts} {filteredRecord.sunrise_ts} {filteredRecord.sunset_ts} {filteredRecord.moonrise_ts}
-        {filteredRecord.moonset_ts}  {filteredRecord.moon_phase} {filteredRecord.moon_phase_lunation}
+        <p>The ts is currently {filteredRecord.ts}. Sunrise today is {filteredRecord.sunrise_ts} with sunset expected to be around {filteredRecord.sunset_ts}.
+            Moonrise is currently forecast for {filteredRecord.moonrise_ts} this evening, with moonset at {filteredRecord.moonset_ts} tomorrow morning. The moon is
+            currently in {filteredRecord.moon_phase} of its moon phase, with its moon phase lunation being {filteredRecord.moon_phase_lunation}.
         </p>
-        <p>Additional Info: {filteredRecord.rh} {filteredRecord.pres} {filteredRecord.pop} {filteredRecord.slp}
-            {filteredRecord.dewpt} {filteredRecord.max_dhi}
+        <p>Additional Info: The rh is: {filteredRecord.rh}. Pres is {filteredRecord.pres}. Pop is {filteredRecord.pop}. Slp is {filteredRecord.slp}. Dewpt is
+            {filteredRecord.dewpt}, and max dhi is {filteredRecord.max_dhi}
         </p>
       </div>
 
