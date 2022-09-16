0bd179cfc8821606556284469970751c632f5f45
AJWest22 <abiwestcott@gmail.com>
2022-09-16 07:13:34 +0000

    Remove unnessecary text from CityWeather

--- weather-app/src/components/CityWeather.js
+++ weather-app/src/components/CityWeather.js
@@ -23,25 +23,11 @@ export default function CityWeather() {
       </div>
 
       <div id="alignment">
-        <p>Temperature of {filteredRecord.temp} with a high temperature of {filteredRecord.high_temp} with 
-            a max temperature of {filteredRecord.max_temp} with a absolute max temperature of {filteredRecord.app_max_temp}.
-            A low temperature of {filteredRecord.low_temp} is expected, with a min temperature of {filteredRecord.min_temp}
-            UV is expected to be {filteredRecord.uv}. Ozone is currently {filteredRecord.ozone}.
-            The current wind direction is {filteredRecord.wind_cdir}, with the full cdir expected to be {filteredRecord.wind_cdir_full}. 
-            The wind direction is today expected to be from {filteredRecord.wind_dir} with gusts of {filteredRecord.wind_gust_spd}.
-            {filteredRecord.wind_spd}. The high clouds are currently located at {filteredRecord.clouds_hi}, with the mid clouds at 
-            {filteredRecord.clouds_mid}. The low clouds are currently located at {filteredRecord.clouds_low}. Visibility is 
-            currently {filteredRecord.vis}. Precipitation is {filteredRecord.precip} with snow currently at {filteredRecord.snow}. 
-            The current snow depth is {filteredRecord.snow_depth}. The ts is currently {filteredRecord.ts}. Sunrise today 
-            is {filteredRecord.sunrise_ts} with sunset expected to be around {filteredRecord.sunset_ts}. Moonrise is currently forecast 
-            for {filteredRecord.moonrise_ts} this evening, with moonset at {filteredRecord.moonset_ts} tomorrow morning. The moon is
-            currently in {filteredRecord.moon_phase} of its moon phase, with its moon phase lunation being {filteredRecord.moon_phase_lunation}.
-            Additional Info: The rh is: {filteredRecord.rh}. Pres is {filteredRecord.pres}. Pop is {filteredRecord.pop}. Slp is {filteredRecord.slp}. 
-            Dewpt is currently {filteredRecord.dewpt}.
+        <p>Temperature of {filteredRecord.temp} with a max temperature of {filteredRecord.max_temp}.
+            A low temperature of {filteredRecord.low_temp} is expected, with a min temperature of 
+            {filteredRecord.min_temp}
         </p>
-        <img src={icon} alt="todays weather" height="150x px" width="150px"></img>
-      </div>
-      <div>
+        <img src={icon} alt="todays weather" height="150px" width="150px"></img>
       </div>
     </div>
   )
