08b2fd87e66090cc367425370cef39e3f03ba7f9
AJWest22 <abiwestcott@gmail.com>
2022-09-16 16:01:51 +0000

    Update react router

---
+++ weather-app/src/pages/DayTwo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import info from '../weather-data.json'
+
+
+export default function DayTwo() {
+  return (
+    <div>
+        <h2>Tomorrow's Forecast</h2>
+    </div>
+  )
+}
