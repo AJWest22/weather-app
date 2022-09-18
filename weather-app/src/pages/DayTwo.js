63999294e6d0e5797b1f14df965a7185b91c2161
AJWest22 <abiwestcott@gmail.com>
2022-09-18 11:28:16 +0000

    Update group styling on mobile

--- weather-app/src/pages/DayTwo.js
+++ weather-app/src/pages/DayTwo.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Route, Routes, useNavigate} from 'react-router-dom'
 import info from '../weather-data.json'
 import'../components/GroupOne.js'
-import filteredRecord from '../components/GroupOne.js'
 
 
 export default function DayTwo() {
+  const [detailedRecord, setDetailedRecord] = useState({});
+
+  useEffect(() => {
+    const detail = info.data.filter((it) => it.wind_dir === 289);
+    setDetailedRecord(detail[0]);
+  }, []);
   return (
     <div>
       <h3>Tomorrow's Forecast</h3>
-      <p>{filteredRecord.wind_dir}</p>
+      <p className="group-text">{detailedRecord.temp}</p>
     </div>
   )
 }
